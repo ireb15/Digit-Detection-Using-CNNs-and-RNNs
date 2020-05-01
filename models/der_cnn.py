aad67d0800aebb0b5a518538d125fbe89f5d5731
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-05-01 18:51:13 +1200

    The code in this commit is example code from the COMPSYS 302 labs from which we will derive two new models.

---
+++ models/der_cnn.py
@@ -0,0 +1,4 @@
+# The following code makes up the Derived CNN (der_CNN) model. It is derived from the example CNN model from the COMPSYS
+# 302 labs (found in conv.py).
+
+
---
+++ models/der_rnn.py
@@ -0,0 +1,4 @@
+# The following code makes up the Derived RNN (der_RNN) model. It is derived from the example RNN model from the COMPSYS
+# 302 labs (found in rnn_conv.py).
+
+
