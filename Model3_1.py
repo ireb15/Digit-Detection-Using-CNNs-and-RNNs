4b3d4b8301ac318525348dd426d8d83aaaf58e4c
ahad2423 <asul867@aucklanduni.ac.nz>
2020-05-04 21:08:25 +1200

    Update Model3_1.py

--- Model3_1.py
+++ Model3_1.py
@@ -10,12 +10,17 @@ import time
  
 from torchvision import datasets
 
+# define constants
+NUM_EPOCHS = 3
+BATCH_SIZE = 10
+LEARNING_RATE = 0.01
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5,), (0.5,))]
 )
 
-# get the data
+# get the data from MNIST
 trainset = datasets.MNIST(
     root = './data',
     train = True,
@@ -25,7 +30,7 @@ trainset = datasets.MNIST(
  
 trainloader = torch.utils.data.DataLoader(
     trainset, 
-    batch_size = 4,
+    batch_size = 10,
     shuffle = True
 )
  
@@ -38,10 +43,11 @@ testset = datasets.MNIST(
  
 testloader = torch.utils.data.DataLoader(
     testset, 
-    batch_size = 4,
+    batch_size = 10,
     shuffle = False
 )
 
+"""
 for batch_1 in trainloader:
     batch = batch_1
     break
@@ -57,6 +63,7 @@ for i in range (batch[0].shape[0]):
     plt.title(int(batch[1][i]))
     plt.savefig('digit_mnist.png')
 plt.show()
+"""
 
 class Net(nn.Module):
     def __init__(self):
@@ -70,63 +77,79 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(in_features=84, out_features=10)
  
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = F.tanh(self.conv1(x))
         x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
+        x = F.tanh(self.conv2(x))
         x = F.max_pool2d(x, kernel_size=2)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-net = Net().to(device)
+net = Net()
 print(net)
 
-# loss function
-criterion = nn.CrossEntropyLoss()
-# optimizer
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# loss function and optimizer
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=0.9)
 
-def train(net):
-    start = time.time()
-    for epoch in range(10): # no. of epochs
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # data pixels and labels to GPU if available
-            inputs, labels = data[0].to(device, non_blocking=True), data[1].to(device, non_blocking=True)
-            # set the parameter gradients to zero
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            # propagate the loss backward
-            loss.backward()
-            optimizer.step()
- 
-            # print for mini batches
-            running_loss += loss.item()
-            if i % 5000 == 4999:  # every 5000 mini batches
-                print('[Epoch %d, %5d Mini Batches] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss/5000))
-                running_loss = 0.0
-    end = time.time()
- 
-    print('Done Training')
-    print('%0.2f minutes' %((end - start) / 60))
-    
-train(net)
+# if GPU is available, then use GPU, else use CPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+net.to(device)
 
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        inputs, labels = data[0].to(device, non_blocking=True), data[1].to(device, non_blocking=True)
+# function to calculate accuracy
+def calc_acc(loader):
+    correct = 0
+    total = 0
+    for data in loader:
+        inputs, labels = data[0].to(device), data[1].to(device)
         outputs = net(inputs)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        
+    return ((100*correct)/total)
+
+def train():
+    epoch_loss = []
+    train_acc = []
+    test_acc = []
+    for epoch in range(NUM_EPOCHS):
+        running_loss = 0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
  
-print('Accuracy of the network on test images: %0.3f %%' % (
-    100 * correct / total))
+            # set parameter gradients to zero
+            optimizer.zero_grad()
+ 
+            # forward pass
+            outputs = net(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+ 
+            running_loss += loss.item()
+ 
+        epoch_loss.append(running_loss/15000)
+        train_acc.append(calc_acc(trainloader))
+        test_acc.append(calc_acc(testloader))
+        print('Epoch: %d of %d, Train Acc: %0.3f, Test Acc: %0.3f, Loss: %0.3f'
+              % (epoch+1, NUM_EPOCHS, train_acc[epoch], test_acc[epoch], running_loss/15000))
+        
+    return epoch_loss, train_acc, test_acc
+
+#start training 
+start = time.time()
+epoch_loss, train_acc, test_acc = train()
+end = time.time()
+ 
+print('%0.2f minutes' %((end - start) / 60))
+
+#show the output graph
+plt.figure()
+plt.plot(epoch_loss)
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.savefig('loss_v_ephoch.png')
+plt.show()
