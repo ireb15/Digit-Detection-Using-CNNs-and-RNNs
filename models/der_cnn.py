434507a07d24155e0c081f574a61def57aea02fd
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-05-05 12:15:23 +1200

    der_CNN v5 - added extra dropout with p=0.1, results were worse than v4

--- models/der_cnn.py
+++ models/der_cnn.py
@@ -12,13 +12,14 @@ def silu(input):
 class der_CNN(nn.Module):
     def __init__(self):
         super(der_CNN, self).__init__()
-        # 1: input channels 32: output channels, 3: kernel size, 1: stride
+        # Convolutional layers: input channels, output channels, kernel size, stride
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        # Dropouts: probability
         # "Filter out" some of the input based on their probability of being zeroed.
         self.dropout1 = nn.Dropout2d(0.5)
         self.dropout2 = nn.Dropout2d(0.25)
-        #self.dropout3 = nn.Dropout2d(0.1)
+        self.dropout3 = nn.Dropout2d(0.1)
         # Fully connected layers: input size, output size
         self.fc1 = nn.Linear(9216, 120)
         self.fc2 = nn.Linear(120, 84)
@@ -39,7 +40,7 @@ class der_CNN(nn.Module):
         input = silu(input)
         input = self.dropout2(input)
         input = self.fc2(input)
-        #input = self.dropout3(input)
+        input = self.dropout3(input)
         input = self.fc3(input)
         output = func.log_softmax(input, dim=1)
         return output
